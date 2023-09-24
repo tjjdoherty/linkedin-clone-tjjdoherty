22bc243c81f506d8cfcc0cce58e0cfb26b4ee9d7
Thomas Doherty <thomas_doherty92@hotmail.com>
2023-09-24 18:28:33 -0400

    LinkedIn Clone. Just setting up first login page folders and files no content yet. Set up firebaseConfig too

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <p>Hello world</p>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/firebaseConfig.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAHX2KypsVoLtkqYOiiEc2XCJdY0JGG88A",
+  authDomain: "linkedin-clone-tjjdoherty.firebaseapp.com",
+  projectId: "linkedin-clone-tjjdoherty",
+  storageBucket: "linkedin-clone-tjjdoherty.appspot.com",
+  messagingSenderId: "888162640053",
+  appId: "1:888162640053:web:51ee5268579300b3f99efc"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+
