b94760c5eefb2d036db1ec010187b03c83c7de19
Thomas Doherty <thomas_doherty92@hotmail.com>
2023-10-02 08:12:54 -0400

    added authentication states, loader and navigation from login and signup to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,22 @@
-import React from 'react';
+import { React, useEffect } from 'react';
 import HomeComponent from "../components/HomeComponent";
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '../firebaseConfig';
+import { useNavigate } from "react-router-dom";
+import Loader from "../components/common/Loader";
+
+// onAuthStateChanged - if there is no longer an access token, or it doesn't exist in the first place, navigate the user back to the login (/) page
 
 export default function Home() {
+    let navigate = useNavigate();
+    useEffect(() => {
+        onAuthStateChanged(auth, (response) => {
+            if (!response?.accessToken) {
+                navigate("/");
+            } else {
+                return <Loader />;
+            }
+        });
+    }, []);
     return <HomeComponent />;
 }
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import { React, useEffect } from 'react';
 import LoginComponent from "../components/LoginComponent";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from '../firebaseConfig';
+import { useNavigate } from "react-router-dom";
+import Loader from "../components/common/Loader";
+
+// onAuthStateChange - if an accessToken is found (because a user provided valid credentials) then navigate them to the home page, now they're signed into the portal
 
 export default function Login() {
+    let navigate = useNavigate();
+    useEffect(() => {
+        onAuthStateChanged(auth, (response) => {
+            if (response?.accessToken) {
+                navigate("/home");
+                console.log("Logged in, no loader")
+            } else {
+                return <Loader />;
+            }
+        });
+    }, []);
     return <LoginComponent />;
 }
