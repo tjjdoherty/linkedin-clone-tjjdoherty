3f172e3f5b9fd95d0dc54470e2063d2eaa04b673
Thomas Doherty <thomas_doherty92@hotmail.com>
2023-10-31 09:27:21 -0400

    added extra fields for profile edit. the logic is not there in firebase yet, only front end fields. 5 28 00

--- src/components/common/ProfileEdit/index.jsx
+++ src/components/common/ProfileEdit/index.jsx
@@ -3,15 +3,13 @@ import { editProfile } from "../../../api/FirestoreAPI";
 import "./index.scss"
 
 export default function ProfileEdit({ onEdit, currentUser }) {
-    const [editInputs, setEditInputs] = useState({});
+    const [editInputs, setEditInputs] = useState(currentUser);
     const getInput = (event) => {
         // console.log(event);
         let { name, value } = event.target;
         let input = { [name]: value };
         setEditInputs({ ...editInputs, ...input });
     };
-
-    // console.log(editInputs);
 
     // updateProfileData sends all the updates to Firestore and onEdit is the condition that toggles between the profile and profile edit page.
 
@@ -20,45 +18,87 @@ export default function ProfileEdit({ onEdit, currentUser }) {
         await onEdit();
     }
 
+    // console.log(currentUser);
+
     return (
         <div className="profile-card">
             <div className="edit-btn">
                 <button onClick={onEdit}>Go back</button>
             </div>
             <div className="profile-edit-inputs">
+                <label>Name</label>
                 <input 
                     className="common-input" 
                     placeholder="Name" 
                     onChange={getInput}
                     name='name'
+                    value={editInputs.name}
                 />
-
+                <label>Headline</label>
                 <input 
                     className="common-input" 
                     placeholder="Headline" 
                     onChange={getInput} 
                     name='headline'
-                />
-
+                    value={editInputs.headline}
+                />        
+                <label>Location</label>
                 <input 
                     className="common-input" 
                     placeholder="Location" 
                     onChange={getInput}
                     name='location'
+                    value={editInputs.location}
                 />
-
+                <label>City</label>
+                <input 
+                    className="common-input" 
+                    placeholder="City" 
+                    onChange={getInput}
+                    name='city'
+                    value={editInputs.city}
+                />
+                <label>Company</label>
                 <input 
                     className="common-input" 
                     placeholder="Company" 
                     onChange={getInput} 
                     name='company'
+                    value={editInputs.company}
                 />
-                
+                <label>Industry</label>
+                <input 
+                    className="common-input" 
+                    placeholder="Industry" 
+                    onChange={getInput} 
+                    name='industry'
+                    value={editInputs.industry}
+                />
+                <label>College or University</label>
                 <input 
                     className="common-input" 
                     placeholder="College or University" 
                     onChange={getInput} 
                     name='college'
+                    value={editInputs.college}
+                />
+                <label>Website</label>
+                <input 
+                    className="common-input" 
+                    placeholder="Website" 
+                    onChange={getInput} 
+                    name='website'
+                    value={editInputs.website}
+                />
+                <label>About</label>
+                <textarea 
+                    className="common-textArea"
+                    placeholder="About Me" 
+                    onChange={getInput} 
+                    rows={5}
+                    cols={5}
+                    name="aboutMe" 
+                    value={editInputs.aboutMe} 
                 />
             </div>
             <div className="save-container">
