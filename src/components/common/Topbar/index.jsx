48a6d0530002c0c5110299a36bf25be13ff4ba91
Thomas Doherty <107805731+tjjdoherty@users.noreply.github.com>
2023-12-19 09:13:39 -0500

    completed search functionality in topbar

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './index.scss';
 import LinkedInLogo from '../../../assets/linkedInLogo.png';
@@ -8,12 +8,15 @@ import { IoMdNotificationsOutline } from 'react-icons/io';
 import user from '../../../assets/userIcon.png';
 import ProfilePopup from '../ProfilePopup';
 import SearchUsers from '../SearchUsers';
+import { getAllUsers } from '../../../api/FirestoreAPI';
 
 //const goToRoute - this is the navigate function for all of the top bar icons. Line 2 import useNavigate from react router dom...no need for the helpers/useNavigate?
 
 export default function Topbar({ currentUser }) {
     const [profilePopupOpen, setProfilePopupOpen] = useState(false);
     const [isSearch, setIsSearch] = useState(false);
+    const [users, setUsers] = useState([]);
+    const [filteredUsers, setFilteredUsers] = useState([]);
     const [searchInput, setSearchInput] = useState('');
 
     let navigate = useNavigate();
@@ -21,6 +24,35 @@ export default function Topbar({ currentUser }) {
     const goToRoute = (route) => {
         navigate(route);
     }
+
+    useEffect(() => {
+        getAllUsers(setUsers);
+    })
+
+    const handleSearch = () => {
+        if(searchInput !== '') {
+            let searched = users.filter((user) => {
+                return Object.values(user)
+                .join('')
+                .toLowerCase()
+                .includes(searchInput.toLowerCase())
+            });
+            setFilteredUsers(searched);
+        }
+        else {
+            setFilteredUsers(users);
+        }
+    };
+
+    // this useEffect will run whenever search input changes, timeout is just to refresh it every 1 second
+
+    useEffect(() => {
+        let debounced = setTimeout(() => {
+            handleSearch();
+        }, 1000)
+
+        return () => clearTimeout(debounced);
+    }, [searchInput])
 
     return (
         <div className="topbar-main">
@@ -67,7 +99,17 @@ export default function Topbar({ currentUser }) {
                 <></>
             ) : (
                 <div className="search-results">
-
+                    {filteredUsers.length === 0 ? (
+                        <div className="search-inner">No Results Found</div>
+                    ) : (
+                        filteredUsers.map((user) => (
+                            <div className="search-inner">
+                                <img src={user.imageLink} />
+                                <p classname="name">{user.name}</p>
+                                
+                            </div>
+                            ))
+                        )}
                 </div>
             )}
 
